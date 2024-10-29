0e6705a7ca3d57825b3541a5776b0e502e86c8b8
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-10-28 20:05:00 -0400

    random placement of ships is working

--- js/script.js
+++ js/script.js
@@ -1,57 +1,91 @@
 //Referenced Element Constants
 const display = document.querySelector(".display");
-const grids = document.querySelectorAll(".grid");
 const pGrid = document.querySelector("#pGrid");
 const cGrid = document.querySelector("#cGrid");
-const playerScore = document.querySelector("#playerScoresheet");
-const computerScore = document.querySelector("#computerScoresheet");
+const pScore = document.querySelector("#playerScoresheet");
+const cScore = document.querySelector("#computerScoresheet");
 const gameBtn = document.querySelector("#gameButton");
 
 //Variables
 const gridSize = 10; //how man cell rows and cols on each board
-const carrierL = 5;
-const battleshipL = 4; //length of
-const destroSubL = 3; //ships
-const pBoutL = 2;
 let message = (display.textContent = "Press Start Game");
 const pGameBoard = []; //starting grid arrays
 const cGameBoard = [];
-let gameState = null;
 const ships = [
-    {name: 'carrier', length: 5},
-    {name: 'battleship', length: 4},
-    {name: 'destroyer', length: 3},
-    {name: 'submarine', length: 3},
-    {name: 'patrol boat', length: 2}
-]
-
-
-
-//TODO handle placing of ships at beggining of game(player and computer)
-//TODO handle picking of cells to guess (player and computer)
+  { name: "carrier", length: 5 },
+  { name: "battleship", length: 4 },
+  { name: "destroyer", length: 3 },
+  { name: "submarine", length: 3 },
+  { name: "patrol-boat", length: 2 },
+];
 
 //Functions
-const createGameBoard = gameBoard => {
-    for (let row = 0; row < gridSize; row++) {
-      let currentRow = [];
-      for (let col = 0; col < gridSize; col++) {
-        currentRow.push("water");
-      }
-      gameBoard.push(currentRow);
+const createGameBoard = (gameBoard) => {
+  for (let row = 0; row < gridSize; row++) {
+    let currentRow = [];
+    for (let col = 0; col < gridSize; col++) {
+      currentRow.push(null);
     }
+    gameBoard.push(currentRow);
   }
+};
 
-const createGrid = gridEl => {
-    //Create Grids Function
-    for (let row = 0; row < gridSize; row++) {
-      for (let col = 0; col < gridSize; col++) {
-        let cell = document.createElement("div");
-        cell.classList.add(gridEl.id, "cell", 'water');
-        cell.id = `${gridEl.id}-${row},${col}`; //data attributs for finding location of cell
-        gridEl.appendChild(cell);
-      }
+const createGrid = (gridEl) => {
+  //Create Grids Function
+  for (let row = 0; row < gridSize; row++) {
+    for (let col = 0; col < gridSize; col++) {
+      let cell = document.createElement("div");
+      cell.classList.add(gridEl.id, "cell", "water");
+      cell.id = `${gridEl.id}-${row},${col}`; //data attributs for finding location of cell
+      gridEl.appendChild(cell);
     }
   }
+};
+
+const placeShips = (gameBoard) => {
+  ships.forEach((ship) => {
+    let isPlaced = false;
+    while (!isPlaced) {
+      let orientation = Math.floor(Math.random() * 2); //orientation will either be 0 or 1
+      let startRow = Math.floor(Math.random() * 10); //picks 0-9
+      let startCol = Math.floor(Math.random() * 10); //picks 0-9
+      console.log(orientation);
+      if (orientation === 0) {
+        //horizontal
+        if (startCol + ship.length < 10) {
+          let overlap = false;
+          for (let i = 0; i < ship.length; i++) {
+            if (gameBoard[startRow][startCol + i]) {
+              overlap = true;
+            }
+          }
+          if (!overlap) {
+            for (let i = 0; i < ship.length; i++) {
+              gameBoard[startRow][startCol + i] = ship.name;
+            }
+            isPlaced = true;
+          }
+        }
+      } else {
+        //vertical
+        if (startRow + ship.length < 10) {
+          let overlap = false;
+          for (let i = 0; i < ship.length; i++) {
+            if (gameBoard[startRow + i][startCol]) {
+              overlap = true;
+            }
+          }
+          if (!overlap) {
+            for (let i = 0; i < ship.length; i++) {
+              gameBoard[startRow + i][startCol] = ship.name;
+            }
+            isPlaced = true;
+          }
+        }
+      }
+    }
+  });
+};
 
 function buttonClickHandler(event) {
   gameState = "setup";
@@ -61,12 +95,14 @@ function buttonClickHandler(event) {
 }
 
 function init() {
-    //initial state function
-    createGameBoard(pGameBoard); //creates player game board
-    createGameBoard(cGameBoard); //creates computer game board
-    createGrid(pGrid); //creates player grid
-    createGrid(cGrid); //creates computer grid
-  }
+  //initial state function
+  createGameBoard(pGameBoard); //creates player game board
+  createGameBoard(cGameBoard); //creates computer game board
+  createGrid(pGrid); //creates player grid
+  createGrid(cGrid); //creates computer grid
+  placeShips(cGameBoard);
+  placeShips(pGameBoard);
+}
 
 //Fill out scoresheets
 //allow user to place ships
@@ -75,5 +111,4 @@ function init() {
 init(); //calls function when page is opened
 
 //Event Listeners
-//grids.forEach((cell) => cell.addEventListener("click", gridClickHandler));
 gameBtn.addEventListener("click", buttonClickHandler);
